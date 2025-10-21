1e4c25e0336e8a0bd048854832b1a051ee243eee
awei <xaweiouo@gmail.com>
2025-10-21 15:02:50 +0800

    修正footer logo路徑

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ export function footer(){document.querySelector('footer').innerHTML=
 `
 <div class="w-1410px margin-x-auto d-flex justify-between align-center">
     <a href="./index.html">
-    <img src="/asset/logo.svg" class="logo">
+    <img src="../asset/logo.svg" class="logo">
     </a>
 
     <p>Copyrights © 2025, Wayne , All Rights Reserved , This Website is Just for Job Hunting and Interview </p>
